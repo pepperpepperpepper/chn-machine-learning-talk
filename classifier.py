f360957efbad4116e9f8c1af3f9dd3169b907542
Sam Brotherton <sbrother@gmail.com>
2015-01-12 18:46:01 -0800

    ready for talk

--- classifier.py
+++ classifier.py
@@ -1,4 +1,9 @@
-"""Given a list of labeled files, learns a Naive-Bayes classifier."""
+"""A simple Naive-Bayes classifier designed to train on any number of features,
+and then classify documents into one of two mutually exclusive categories.
+Implements the algorithm described for spam filtering here:
+http://en.wikipedia.org/wiki/Naive_Bayes_classifier#Document_classification
+"""
+
 from collections import defaultdict
 from math import log, exp
 
@@ -7,29 +12,28 @@ class NaiveBayesClassifier(object):
         self.label_feature_lookup = defaultdict(lambda: defaultdict(int))
         self.label_total_feature_counts = defaultdict(int)
         self.label_total_document_counts = defaultdict(int)
-        self.total_documents = 0
 
     def train(self, labeled_features):
         """Accepts a list of labeled features -- tuples of format (label,
         feature_vector), and learns feature weights"""
         for label, feature_vec in labeled_features:
             self.label_total_document_counts[label] += 1
-            self.total_documents += 1
             for feature in feature_vec:
                 self.label_feature_lookup[label][feature] += 1
                 self.label_total_feature_counts[label] += 1
         self.all_labels = self.label_total_document_counts.keys()
 
     def classify(self, feature_vec, label1, label2):
+        '''This function '''
         total_weight = 0
         for feature in feature_vec:
-            p_feature_given_label1 = ((self.label_feature_lookup[label1][feature]+1)/
-                                      (float(self.label_total_feature_counts[label1])+1))
-            p_feature_given_label2 = ((self.label_feature_lookup[label2][feature]+1)/
-                                      (float(self.label_total_feature_counts[label2])+1))
+            p_feature_given_label1 = ((self.label_feature_lookup[label1][feature] + 1.0)/
+                                      (self.label_total_feature_counts[label1] + 1.0))
+            p_feature_given_label2 = ((self.label_feature_lookup[label2][feature] + 1.0)/
+                                      (self.label_total_feature_counts[label2] + 1.0))
             total_weight += log(p_feature_given_label1/p_feature_given_label2)
-        prior_factor = log((self.label_total_document_counts[label1]+1.0)/
-                           (self.label_total_document_counts[label2]+1.0))
+        prior_factor = log((self.label_total_document_counts[label1] + 1.0)/
+                           (self.label_total_document_counts[label2] + 1.0))
         if prior_factor + total_weight > 0:
             return label1
         else:
--- evaluate_classifier.py
+++ evaluate_classifier.py
@@ -5,7 +5,6 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-
 if __name__ == '__main__':
     train, test = get_train_test_split(os.path.join(ROOT_DIR, 'data', 'emails'), 0.6)
     label_lookup = get_label_lookup(os.path.join(ROOT_DIR, 'data', 'labels.txt'))
@@ -16,6 +15,22 @@ if __name__ == '__main__':
         x in train]
     nb_classifier.train(training_data)
 
+    true_positive = true_negative = false_positive = false_negative = 0
     for filename in test:
-        print nb_classifier.classify(features_from_file(os.path.join(ROOT_DIR, 'data', 'emails', filename)), '0', '1')
-    
+        predicted_label = nb_classifier.classify(features_from_file(
+            os.path.join(ROOT_DIR, 'data', 'emails', filename)), 'spam', 'not_spam')
+        if predicted_label == 'spam' and label_lookup[filename] == 'spam':
+            true_positive += 1
+        if predicted_label == 'not_spam' and label_lookup[filename] == 'not_spam':
+            true_negative += 1
+        if predicted_label == 'spam' and label_lookup[filename] == 'not_spam':
+            false_positive += 1
+        if predicted_label == 'not_spam' and label_lookup[filename] == 'spam':
+            false_negative += 1
+
+    print "True Positives:", true_positive
+    print "True Negatives:", true_negative
+    print "False Positives:", false_positive
+    print "False Negatives:", false_negative
+    print "Precision:", float(true_positive)/(true_positive+false_positive)
+    print "Recall:", float(true_positive)/(true_positive+false_negative)
